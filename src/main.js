d2f63e2aee2a326365b2694e2cbb8fbada7a736c
fechen88 <utkevin2@gmail.com>
2023-05-24 11:25:19 -0500

    add directives for lazy loading images

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 
 import '@/styles/common.scss'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 //test api function
 // import { getCategory } from './apis/testAPI'
 // getCategory().then(res => console.log(res))
@@ -15,3 +17,22 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//global directive config
+app.directive('img-lazy',{
+    mounted(el,binding){
+        //el: element to bind
+        //binding: binding.value which is the value berhind =
+        console.log(el,binding.value)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if (isIntersecting){
+                    //entering window
+                    el.src = binding.value
+                }
+            },
+        )
+    }
+})
