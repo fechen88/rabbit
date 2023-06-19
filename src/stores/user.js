167e5b1127b1af9b49088ef41a5ca9ceaae94bb5
fechen88 <utkevin2@gmail.com>
2023-06-19 16:09:35 -0500

    clear user info upon logging out

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,8 +11,12 @@ export const useUserStore = defineStore('user', () => {
         const res = await loginAPI({ account, password })
         userInfo.value = res.result
     }
+    //3.退出时清除用户信息
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
     //3.以对象格式把state和action return
-    return { userInfo, getUserInfo }
+    return { userInfo, getUserInfo, clearUserInfo }
 },{
     persist: true,
 })
